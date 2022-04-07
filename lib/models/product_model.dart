9fdd489049affed4c9aa0f386ad608aa018138d9
alphazen01 <parvejmahmud555@gmail.com>
2022-04-07 22:20:18 +0600

    fetchProducts

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,27 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProductModel {
-  final String id,name,image;
-  final int price;
+  final String id,name,image,quantity;
+  final double price;
   ProductModel({
     required this.id,
     required this.name,
 
     required this.image,
-    required this.price
+    required this.price,
+    required this.quantity
   });
 Map<String,dynamic>toMap()=>{
 "id":id,
 "name":name,
 "price":price,
-"image":image 
+"image":image,
+"quantity":quantity 
 };
 
 factory ProductModel.fromMap(QueryDocumentSnapshot data, String id)=>ProductModel(
   id: id, 
   name: data["products-name"], 
   image: data["products-image"],  
-  price: data["products-price"] 
+  price: data["products-price"],
+  quantity: data["products-quantity"] 
   );
 
 
